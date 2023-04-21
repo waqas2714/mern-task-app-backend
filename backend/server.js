5e38d561d33d7d5826c269bd1dd01fa15098655c
waqas2714 <131449899+waqas2714@users.noreply.github.com>
2023-04-21 16:49:23 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ then(app.listen(PORT,()=>{
 )
 // Middleware functions
 app.use(cors({
-    origin :["http://localhost:3000/"]
+    origin :["http://localhost:3000"]
 }))
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
